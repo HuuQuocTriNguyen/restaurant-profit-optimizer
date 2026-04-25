fe6aaf1a5955b74e610d88e48fe25378f9ca16e2
Christopher nguyen <chrisnguyen1304@ChristophersMBP.lan>
2026-04-25 12:11:17 +0700

    excel support

--- features/utils/data_cleaning.py
+++ features/utils/data_cleaning.py
@@ -1,8 +1,43 @@
 import pandas as pd
 from features.utils.find_column import find_column
 
-def data_clean(file_path):
+def data_clean_csv(file_path):
     df = pd.read_csv(file_path, encoding="ISO-8859-1")
+
+    df.columns = df.columns.str.strip().str.lower()
+
+    date_col = find_column(df, ["date", "time"])
+    product_col = find_column(df, ["product", "item", "name"])
+    price_col = find_column(df, ["price", "unit price"])
+    quantity_col = find_column(df, ["quantity", "qty"])
+
+    if date_col:
+        df[date_col] = pd.to_datetime(df[date_col], errors="coerce")
+        df = df[df[date_col].notna()]
+
+    if price_col:
+        df[price_col] = pd.to_numeric(df[price_col], errors="coerce")
+
+    if quantity_col:
+        df[quantity_col] = pd.to_numeric(df[quantity_col], errors="coerce")
+
+    if product_col:
+        df[product_col] = df[product_col].astype(str).str.lower().str.strip()
+
+    df = df.dropna(subset=[price_col, quantity_col])
+
+    df["revenue"] = df[price_col] * df[quantity_col]
+
+    return df, {
+        "date": date_col,
+        "product": product_col,
+        "price": price_col,
+        "quantity": quantity_col
+    }
+    
+
+def data_clean_excel(file):
+    df = pd.read_excel(file, encoding="ISO-8859-1")
 
     df.columns = df.columns.str.strip().str.lower()
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from features.utils.data_cleaning import data_clean
+from features.utils.data_cleaning import data_clean_csv, data_clean_excel
 from features.utils.extract import top_product, total_revenue, sales_per_day, total_orders, worst_products
 from features.hours_insight import sales_by_time_of_day, average_sales_by_time_of_day
 
@@ -8,10 +8,11 @@ st.title("Restaurant Profit Optimizer")
 
 st.sidebar.header("Controls")
 
-file = st.sidebar.file_uploader("Upload CSV")
+file_csv = st.sidebar.file_uploader("Upload CSV")
+file_excel = st.sidebar.file_uploader("Upload Excel")
 
-if file:
-    df, cols = data_clean(file)   
+if file_csv:
+    df, cols = data_clean_csv(file_csv)   
 
     data = average_sales_by_time_of_day(df, cols["date"])
     best_time = data.idxmax()
@@ -39,3 +40,32 @@ if file:
     st.subheader("Average Revenue by Time of Day")
     st.line_chart(average_sales_by_time_of_day(df, cols["date"]))
 
+
+if file_excel:
+    df, cols = data_clean_excel(file_excel)   
+
+    data = average_sales_by_time_of_day(df, cols["date"])
+    best_time = data.idxmax()
+    worst_time = data.idxmin()
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Total Revenue", total_revenue(df))
+    col2.metric("Orders", total_orders(df, cols["quantity"]))
+    col3.metric("Best period", best_time, "increase staff & Upsell combos")
+    col4.metric("Worst period", worst_time, "Run promotions or discounts", "inverse")
+
+    st.subheader("Top Products")
+    st.bar_chart(top_product(df, cols["product"]), x_label="Product names", y_label="Total Revenue", color="green")
+
+    st.subheader("Products to Reconsider")
+    st.bar_chart(worst_products(df, cols["product"]), x_label="Product names", y_label="Quantity", color="red")
+
+    st.subheader("Average Sales")
+    st.scatter_chart(sales_per_day(df, cols["date"]), x_label="Date", y_label="USD")
+
+    st.subheader("Revenue by Time of Day")
+    st.line_chart(sales_by_time_of_day(df))
+
+    st.subheader("Average Revenue by Time of Day")
+    st.line_chart(average_sales_by_time_of_day(df, cols["date"]))
